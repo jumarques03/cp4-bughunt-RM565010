068e1e82986fbb32c05b9e36478948fc0e49085e
Júlia Marques <jumarques631@gmail.com>
2026-09-10 16:28:58 -0300

    fix: bug11 - torna ClassificacaoIndicativaException unchecked (RuntimeException), removendo throws desnecessario

--- src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -36,7 +36,7 @@ public class Usuario {
         this.creditos = this.creditos - valor;
     }
 
-    public Usuario alugar(Conteudo c) throws ClassificacaoIndicativaException {
+    public Usuario alugar(Conteudo c) {
         if (!c.isDisponivel()) {
             throw new ConteudoIndisponivelException(c.getTitulo() + " não está disponível para aluguel");
         }
